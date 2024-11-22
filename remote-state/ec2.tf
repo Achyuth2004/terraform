b03115098cea94b5f67d620107b99c3a12b3aae0
Achyuth2004 <kk9015@srmist.edu.in>
2024-11-22 11:02:32 +0530

    remote state

---
+++ remote-state/ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "db" {
+    ami= "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = [sg-02538f845572f92ba]
+    instance_type = "t3.micro"
+    tags={
+        Name = "db"
+    }  
+}
