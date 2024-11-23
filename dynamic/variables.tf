2b14ae62f0d8a6615abee12a6df0944fcde0240a
Achyuth2004 <kk9015@srmist.edu.in>
2024-11-23 23:44:42 +0530

    for loop

--- dynamic/variables.tf
+++ dynamic/variables.tf
@@ -1,26 +1,31 @@
 variable "inbound_rules" {
-    type = list(string)
+    type = list(
+        object({
+            port     = number
+            cidr     = list(string)
+            protocol = string
+        })
+    )
     default = [ 
         {
-            port = 22,
-            cidr=["0.0.0.0/0"],
-            protocol="tcp"
+            port     = 22
+            cidr     = ["0.0.0.0/0"]
+            protocol = "tcp"
         },
         {
-            port = 80,
-            cidr=["0.0.0.0/0"],
-            protocol="tcp"
+            port     = 80
+            cidr     = ["0.0.0.0/0"]
+            protocol = "tcp"
         },
         {
-            port = 8080,
-            cidr=["0.0.0.0/0"],
-            protocol="tcp"
+            port     = 8080
+            cidr     = ["0.0.0.0/0"]
+            protocol = "tcp"
         },
         {
-            port = 3306,
-            cidr=["0.0.0.0/0"],
-            protocol="tcp"
+            port     = 3306
+            cidr     = ["0.0.0.0/0"]
+            protocol = "tcp"
         }
      ]
-      
 }
