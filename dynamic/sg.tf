9bef99a9350da790a35b48d91365c1c8108d3597
Achyuth2004 <kk9015@srmist.edu.in>
2024-11-23 23:47:05 +0530

    for loop

--- dynamic/sg.tf
+++ dynamic/sg.tf
@@ -1,27 +1,26 @@
 resource "aws_security_group" "allow_ports" {
-    name = "allow_ports"
-    description = "allowing ports 22,80,8080,3306 access"
+    name        = "allow_ports"
+    description = "Allowing ports 22, 80, 8080, 3306 access"
      
     dynamic "ingress" {
         for_each = var.inbound_rules
         content {
-            from_port        = each.value["port"]
-            to_port          = each.value["port"]
-            protocol         = each.value["protocol"]
-            cidr_blocks      = each.value["cidr"]        
+            from_port   = each.value.port
+            to_port     = each.value.port
+            protocol    = each.value.protocol
+            cidr_blocks = each.value.cidr        
         }
     }
 
     egress {
-        from_port        = 0    #opening all ports
-        to_port          = 0    
-        protocol         = "-1" #opening all protocols
-        cidr_blocks      = ["0.0.0.0/0"]
+        from_port   = 0    # Opening all ports
+        to_port     = 0    
+        protocol    = "-1" # Opening all protocols
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     tags = {
-        Name = "allow_ssh"
+        Name      = "allow_ssh"
         CreatedBy = "ACHYUTH"
     }
 }
-
